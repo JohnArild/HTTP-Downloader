c520b778042b9ad3072e3b3eac2d3a1c507af01b
John Arild Lolland <johnarild+github@protonmail.com>
2019-11-02 19:55:39 +0100

    Moved socket info to class

--- HTTP-Downloader/HTTP-Downloader.cpp
+++ HTTP-Downloader/HTTP-Downloader.cpp
@@ -7,17 +7,37 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+class MySocket
+{
+	WSADATA winSockData_;
+	WORD wVersionRequested_;
+	int errorCode_;
+public:
+	MySocket();
+	~MySocket();
+	int GetErrorCode() { return errorCode_; }
+};
+
+MySocket::MySocket()
+{
+	wVersionRequested_ = MAKEWORD(2, 2); // Winsock DLL version to use
+	errorCode_ = WSAStartup(wVersionRequested_, &winSockData_); //Returns 0 if sucessful
+}
+
+MySocket::~MySocket()
+{
+	WSACleanup();
+}
+
 int main()
 {
-	WSADATA winSockData;
-	WORD wVersionRequested = MAKEWORD(1, 0); // Winsock DLL version to use
-	if (WSAStartup(wVersionRequested, &winSockData)) //Returns 0 if sucessful
+	MySocket wsaSocket;
+	if (wsaSocket.GetErrorCode())
 	{
 		std::cout << "Winsocket did not start" << std::endl;
 	}
 	else 
 		std::cout << "Winsocket started" << std::endl;
-	WSACleanup(); //terminates use of the Winsock 2 DLL
     std::cout << "Hello World!\n";
 }
 
