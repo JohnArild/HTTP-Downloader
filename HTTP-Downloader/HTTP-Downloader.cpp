c6ac40673c15f26c04e3f838130dc5135e13c55c
John Arild Lolland <johnarild+github@protonmail.com>
2019-11-02 21:41:31 +0100

    Connects to IP without errors.

--- HTTP-Downloader/HTTP-Downloader.cpp
+++ HTTP-Downloader/HTTP-Downloader.cpp
@@ -1,5 +1,10 @@
-/* HTTP-Downloader.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/* HTTP-Downloader.cpp
+* by John Arild Lolland (usn id 060419)
+* 
+* USN fag: DFON2110-1 19H, Oppgave: Nettverk - Nettleser
+*
 * Note that the code is Windows spesific (uses Ws2_32.dll and WinSock2.h).
+* Main source for info was https://docs.microsoft.com/en-us/windows/win32/api/winsock2/
 */
 
 #include <iostream>
@@ -7,13 +12,13 @@
 
 int main()
 {
-	MySocket wsaSocket;
+	MySocket wsaSocket(__TEXT("192.168.8.1"), 80);
+	int errorcode = 0;
+
 	if (wsaSocket.GetErrorCode())
-	{
 		std::cout << "Winsocket did not start" << std::endl;
-	}
 	else 
 		std::cout << "Winsocket started" << std::endl;
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n " << wsaSocket.GetErrorCode();
 }
 
--- HTTP-Downloader/MySocket.h
+++ HTTP-Downloader/MySocket.h
@@ -1,27 +1,64 @@
 #pragma once
 
 #include <WinSock2.h>
+#include <ws2tcpip.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
 class MySocket
 {
 	WSADATA winSockData_;
-	WORD wVersionRequested_;
+	SOCKET ConnectSocket_;
+	WORD wVersionRequested_{ MAKEWORD(2, 2) }; // Winsock DLL version to use
 	int errorCode_;
+	u_short port_{ htons(27015) }; // host port
+	sockaddr_in address_; // host address
+	int ConnectIP(PCWSTR IPaddress, int port);
+	int ConnectSocket();
 public:
-	MySocket();
+	MySocket(PCWSTR IPaddress, int port);
 	~MySocket();
 	int GetErrorCode() { return errorCode_; }
 };
 
-MySocket::MySocket()
+MySocket::MySocket(PCWSTR IPaddress, int port)
 {
-	wVersionRequested_ = MAKEWORD(2, 2); // Winsock DLL version to use
 	errorCode_ = WSAStartup(wVersionRequested_, &winSockData_); //Returns 0 if sucessful
+	// socket() creates a socket that is bound to a specific transport service provider.
+	// AF_INET - The Internet Protocol version 4 (IPv4) address family. 
+	// SOCK_STREAM - Uses TCP for AF_INET
+	// IPPROTO_TCP - The Transmission Control Protocol(TCP)
+	ConnectSocket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (errorCode_) WSACleanup();
+	else if (ConnectSocket_ == INVALID_SOCKET)
+	{
+		errorCode_ = WSAGetLastError();
+		WSACleanup();
+	}
+	errorCode_ = ConnectIP(IPaddress, port);
 }
 
 MySocket::~MySocket()
 {
 	WSACleanup();
+}
+
+int MySocket::ConnectSocket()
+{
+	return 0;
+}
+
+int MySocket::ConnectIP(PCWSTR IPaddress, int port)
+{
+	int errorCode{ 0 };
+	address_.sin_family = AF_INET;
+	InetPton(AF_INET, IPaddress, &address_.sin_addr.s_addr);
+	address_.sin_port = htons(port);
+	if (connect(ConnectSocket_, (SOCKADDR*)&address_, sizeof(address_)))
+	{
+		errorCode = WSAGetLastError();
+		closesocket(ConnectSocket_);
+		WSACleanup();
+	}
+	return errorCode;
 }
