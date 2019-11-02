60df4a1ed571bd122abe998f44e4cd5e269087a9
John Arild Lolland <johnarild+github@protonmail.com>
2019-11-02 19:58:45 +0100

    Moved MySocket class to MySocket.h

--- HTTP-Downloader/HTTP-Downloader.cpp
+++ HTTP-Downloader/HTTP-Downloader.cpp
@@ -3,31 +3,7 @@
 */
 
 #include <iostream>
-#include <WinSock2.h>
-
-#pragma comment(lib, "ws2_32.lib")
-
-class MySocket
-{
-	WSADATA winSockData_;
-	WORD wVersionRequested_;
-	int errorCode_;
-public:
-	MySocket();
-	~MySocket();
-	int GetErrorCode() { return errorCode_; }
-};
-
-MySocket::MySocket()
-{
-	wVersionRequested_ = MAKEWORD(2, 2); // Winsock DLL version to use
-	errorCode_ = WSAStartup(wVersionRequested_, &winSockData_); //Returns 0 if sucessful
-}
-
-MySocket::~MySocket()
-{
-	WSACleanup();
-}
+#include "MySocket.h"
 
 int main()
 {
