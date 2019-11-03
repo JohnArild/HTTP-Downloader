b35abb78638df09ec4222196acccf2ca44c0c364
John Arild Lolland <johnarild+github@protonmail.com>
2019-11-03 17:50:02 +0100

    Fetches http://www.columbia.edu/~fdc/sample.html

--- HTTP-Downloader/HTTP-Downloader.cpp
+++ HTTP-Downloader/HTTP-Downloader.cpp
@@ -12,7 +12,15 @@
 
 int main()
 {
-	MySocket wsaSocket(__TEXT("216.58.207.227"), 80);
+	//Downloads http://www.columbia.edu/~fdc/sample.html 
+	//I chose that site since it does not use https and is plain html
+	//It was also the first result when I searched "plain html site"
+	std::string requestString = ""; // Holds the HTTP request string
+	requestString += "GET /~fdc/sample.html HTTP/1.1\r\n";
+	requestString += "Host: www.columbia.edu\r\n";
+	requestString += "Connection: close\r\n\r\n";
+	MySocket wsaSocket(__TEXT("128.59.105.24"), 80); //IP address of  www.columbia.edu
+
 	int errorcode{ 0 };
 	std::string resultString;
 
@@ -22,12 +30,12 @@ int main()
 	{
 		std::cout << "Winsocket started" << std::endl;
 		std::cout << "Sending string to server" << std::endl;
-		wsaSocket.sendRequest("GET / HTTP/1.1\r\n\r\n", resultString);
+		wsaSocket.sendRequest(requestString, resultString);
 		std::cout << resultString;
 	}
 	std::cout << "Error code: " << wsaSocket.GetErrorCode() << std::endl;
-	std::cout << "Do you want to see the output? ";
 
+	std::cout << "Do you want to see the output? ";
 	std::string answer{ "" };
 	std::cin >> answer;
 	if(answer == "yes" || answer == "y") wsaSocket.printBuffer();
